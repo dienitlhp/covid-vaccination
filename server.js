60b87c7484069c91c156a453bbd6c5ddb05ec30a
Djinn <dienitlhp@gmail.com>
2021-08-06 02:53:20 +0700

    test server

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ app.use(express.static(__dirname + '/dist/covid-vaccination'));
 app.get('/*', function(req,res) {res.sendFile(path.join(__dirname+'/dist/covid-vaccination/index.html'));
 });
 
-portapp.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);
